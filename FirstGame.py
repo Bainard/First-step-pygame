22cc8d79d75cc4965619783f35675f4c1ac87e5d
Ayya <fellmann.hu@gmail.com>
2018-02-20 18:34:42 +0100

    initialisation de pygame: une fenetre vide

---
+++ FirstGame.py
@@ -0,0 +1,10 @@
+import pygame
+from pygame.locals import * # import des constantes dans le script (pourquoi???)
+
+pygame.init()               #initialisation OBLIGATOIRE AU DEBUT!!!!
+
+fenetre = pygame.display.set_mode((640, 480))          #para de la fenetre tulpe = taille en pixel
+
+continuer = 1              #boucle infinie pour garder la fenetre ouverte
+while continuer:
+    continue
