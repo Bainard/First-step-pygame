4f421b57aead889bd1c982836891d10e2a7b5f66
Ayya <fellmann.hu@gmail.com>
2018-02-20 18:52:20 +0100

    réajustement de la fenêtre

--- FirstGame.py
+++ FirstGame.py
@@ -3,7 +3,7 @@ from pygame.locals import * # import des constantes dans le script (pourquoi???)
 
 pygame.init()               #initialisation OBLIGATOIRE AU DEBUT!!!!
 
-fenetre = pygame.display.set_mode((640, 480))          #para de la fenetre tulpe = taille en pixel
+fenetre = pygame.display.set_mode((1298, 679))          #para de la fenetre tulpe = taille en pixel
 
 fond = pygame.image.load("background.png").convert() #chargement image background
 fenetre.blit(fond, (0,0)) #collage du background ??? SDL a etudier
