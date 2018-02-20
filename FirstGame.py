7c5e5575bedb3da2b6d9b0010058f0dbd308e635
Ayya <fellmann.hu@gmail.com>
2018-02-20 18:50:02 +0100

    ajout d'un background

--- FirstGame.py
+++ FirstGame.py
@@ -5,6 +5,11 @@ pygame.init()               #initialisation OBLIGATOIRE AU DEBUT!!!!
 
 fenetre = pygame.display.set_mode((640, 480))          #para de la fenetre tulpe = taille en pixel
 
+fond = pygame.image.load("background.png").convert() #chargement image background
+fenetre.blit(fond, (0,0)) #collage du background ??? SDL a etudier
+
+pygame.display.flip() #rafraichissement de lecran ???
+
 continuer = 1              #boucle infinie pour garder la fenetre ouverte
 while continuer:
     continue
